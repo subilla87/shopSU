e504983bdb60fe629870aa003e038f1aa3a28c32
Santiago Ubilla <santiago_ubilla87@hotmail.com>
2019-05-30 10:30:48 -0500

    cambios 2

---
+++ shopSU/shopSU.Web/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shopSU.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using shopSU.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
