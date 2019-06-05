d7136fe3a8addd9f410616a3b1529924178c3446
Santiago Ubilla <santiago_ubilla87@hotmail.com>
2019-06-04 21:10:26 -0500

    20190604 using shopSU.UIForms.ViewModels;2

---
+++ shopSU/shopSU.UIForms/shopSU.UIForms/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace shopSU.UIForms.Infrastructure
+{
+    using shopSU.UIForms.ViewModels;
+    class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ shopSU/shopSU.UIForms/shopSU.UIForms/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shopSU.UIForms.ViewModels
+{
+    class MainViewModel
+    {
+    }
+}
